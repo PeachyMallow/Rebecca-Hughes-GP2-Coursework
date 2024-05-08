b5d74cca1779c2fc1954f6af2c7187374dd05478
PeachyMallow <rebecca.abby.hughes@gmail.com>
2024-05-08 22:13:58 +0100

    Removed all Sound work

--- Lab1/src/dr_lib_implementation.c
+++
@@ -1,4 +0,0 @@
-// generates dr_wav function bodies for the linker
-
-#define DR_MP3_IMPLEMENTATION
-#include "dr_lib/dr_wav.h"
--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -1,5 +1,6 @@
 
 //#include <iostream>
+
 #include "MainGame.h"
 
 int main(int argc, char** argv) //argument used to call SDL main
